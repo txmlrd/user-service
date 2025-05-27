16034f6ea1b3880e56eadc300818f1a91303621f
txmlrd <gungadhisanjaya@gmail.com>
2025-05-27 21:45:52 +0700

    fix: fixing duplicate uuid

--- app/models/user.py
+++ app/models/user.py
@@ -6,7 +6,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    uuid = db.Column(db.String(36), unique=True, nullable=False, default=str(uuid.uuid4())) 
+    uuid = db.Column(db.String(36), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
     name = db.Column(db.String(50), nullable=False)
     phone = db.Column(db.String(20), unique=True, nullable=True) 
     profile_picture = db.Column(db.String(100), nullable=True) 
